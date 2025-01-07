9d7c48ada80df01156b63aa283f3d079afc7a285
adil ghani <>
2025-01-07 12:24:46 +0000

    filtered_data is used to filter data only for year 2001 and 2010

--- boxplot.R
+++ boxplot.R
@@ -4,3 +4,6 @@ library(dplyr)
 library(scales)  # For controlling y-axis labels
 # Load the dataset
 data <- read.csv("10_Property_stolen_and_recovered.csv")
+# Filter data for 2001 and 2010
+filtered_data <- data %>%
+  filter(Year %in% c(2001, 2010))
